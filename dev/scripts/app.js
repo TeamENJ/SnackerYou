49e69717936ad7d461d5ec3ffa33506b95ab3869
Jordan Guy <jordan.guy.277@gmail.com>
2018-03-12 17:14:02 -0400

    ready to go

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,215 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import axios from 'axios';
-// import MapContainer from './google';
-
-// const googleURL = "https://maps.googleapis.com/maps/api/geocode/json?";
-
-// // Initialize Firebase
-// var config = {
-//     apiKey: "AIzaSyA2f5SWWUhBul0ey99wAJtcEMU_wLCu61Q",
-//     authDomain: "dude-wheres-my-food.firebaseapp.com",
-//     databaseURL: "https://dude-wheres-my-food.firebaseio.com",
-//     projectId: "dude-wheres-my-food",
-//     storageBucket: "dude-wheres-my-food.appspot.com",
-//     messagingSenderId: "351310641968"
-// };
-// firebase.initializeApp(config);
-// const provider = new firebase.auth.GoogleAuthProvider();
-// const auth = firebase.auth();
-
-// class App extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             userText: '',
-//             restaurants: [],
-//             coordinates: {},
-//             username: '',
-//             user: null,
-//             savedRestaurants: []
-//         }
-//         this.signIn = this.signIn.bind(this);
-//         this.signOut = this.signOut.bind(this);
-//         this.handleChange = this.handleChange.bind(this);
-//         this.submit = this.submit.bind(this);
-//         this.getCoords = this.getCoords.bind(this);
-//         this.getSavedRestaurants = this.getSavedRestaurants.bind(this);
-//         // this.getSavedRestaurants();        
-
-//     }
-
-//     signIn() {
-
-//         auth.signInWithPopup(provider)
-//         .then((result) => {
-//             const user = result.user;
-//             this.setState ({
-//                 user
-//             })
-//             console.log(user)
-//         })
-//     }
-//     signOut() {
-//         auth.signOut()
-//         .then(()=> {
-//             this.setState({
-//                 user:null
-//             });
-//         });
-//     }
-//     handleChange(e) {
-//         this.setState({
-//             [e.target.id]: e.target.value
-//         })
-//     }
-//     getSavedRestaurants() {
-//         const dbRef = firebase.database().ref('/restaurants');
-
-//         const restaurantList = [];
-
-//         // dbref.on('value', (snapshot) => {
-//         //     const data = snapshot.val();
-//         //     const state = [];
-//         //     for (let key in data) {
-//         //         data[key].key = key;
-//         //         state.push(data[key]);
-//         //     }
-        
-//         dbRef.on('value', (response) => {
-//             const data = response.val();
-//             const state = [];
-//             for (let key in data) {
-//                 data[key].key = key;
-//                 state.push(data[key]);
-//             }
-
-//             this.setState({
-//                 savedRestaurants: state
-//             });
-//         })
-//     }
-//     componentDidMount () {
-//         auth.onAuthStateChanged((user) =>{
-//             if(user) {
-//                 this.setState({user})
-//             }
-//         });
-//     }
-//     getCoords(address) {
-//         axios
-//             .get(`${googleURL}`, {
-//                 params: {
-//                     key: "AIzaSyDNBpAAUuUkRyioDLQUQW_DZYIb1PiY85Q",
-//                     address: address
-//                 }
-//             })
-//             .then(({ data }) => {
-//                 axios
-//                     .get(`https://developers.zomato.com/api/v2.1/search`, {
-//                         headers: {
-//                             "user-key": `53314a8415a07eafa4656461b1c6272d`
-//                         },
-//                         params: {
-//                             lat: data.results[0].geometry.location.lat,
-//                             lon: data.results[0].geometry.location.lng,
-//                             radius: '500',
-//                             sort: 'real_distance'
-//                         }
-//                     }).then((response) => {
-//                         let newArray = Array.from(this.state.restaurants);
-
-//                         newArray = response.data.restaurants.map(eatingPlace => {
-
-//                             return {
-//                                 name: eatingPlace.restaurant.name,
-//                                 address: eatingPlace.restaurant.location.address,
-//                                 latitude: eatingPlace.restaurant.location.latitude,
-//                                 longitude: eatingPlace.restaurant.location.longitude,
-//                                 // rating: eatingPlace.restaurant.user_rating.aggregate_rating
-//                             };
-//                         });
-
-//                         this.setState({
-//                             restaurants: newArray,
-//                             lat: data.results[0].geometry.location.lat,
-//                             lon: data.results[0].geometry.location.lng,
-//                             coordinates: {
-//                                 lat: data.results[0].geometry.location.lat,
-//                                 lng: data.results[0].geometry.location.lng
-//                             }
-//                         });
-//                     });
-//             });
-//     }
-//     submit(e) {
-//         e.preventDefault();
-
-//         const inputResult = this.state.userText;
-//         const coords = {};
-
-//         this.getCoords(inputResult);
-//     }
-//     render() {
-
-//         return <div>
-//             <div className="logo">
-//               <img src="./public/images/fullLogo.png" />
-//             </div>
-//             <div className="signOut">
-//               {this.state.user ? <div className="clearfix">
-//                   <div className="userStuff">
-//                     <div className="userPhoto">
-//                         <div>
-//                             <img src={this.state.user.photoURL} alt="" />
-//                         </div>
-//                       <span>Welcome, {this.state.user.displayName}!</span>
-//                     </div>
-//                     {/* <form onSubmit={this.submit} className="wrapper">
-//                     <label htmlFor="userSearch">City or Address:</label>
-//                     <input type="text" id="userText" value={this.state.userText} onChange={this.handleChange} />
-//                     <input type="submit" value="Food Me!" />
-//                   </form>
-
-//                   <div id="map" className="map">
-
-//                     <MapContainer locations={this.state.restaurants} coords={this.state.coordinates} />
-//                   </div>     */}
-//                   </div>
-//                 </div> : <div className="wrapper">
-//                 </div>}
-//               {this.state.user ? <button className="authButton logOut" onClick={this.signOut}>
-//                   Sign Out
-//                 </button> : 
-//                 <button className="authButton logIn" onClick={this.signIn}>
-//                   Sign in
-//                 </button>}
-//             </div>
-
-//             {this.state.user ? <div className="search-thing">
-//                 <form onSubmit={this.submit} className="wrapper">
-//                   <label htmlFor="userSearch">City or Address:</label>
-//                   <input type="text" id="userText" value={this.state.userText} onChange={this.handleChange} />
-//                   <input type="submit" value="Food Me!" />
-//                 </form>
-
-//                 <div id="map" className="map">
-                  
-
-//                     <MapContainer locations={this.state.restaurants} coords={this.state.coordinates} userHistory={this.state.savedRestaurants} />
-//                   </div>    
-
-//                 </div>
-//                : <div>
-//               </div>}
-//           </div>;
-//     }
-// }
-
-// ReactDOM.render(<App />, document.getElementById('app'));
-
-//WORKING CODE
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
@@ -248,7 +36,6 @@ class App extends React.Component {
         this.getCoords = this.getCoords.bind(this);
         this.getSavedRestaurants = this.getSavedRestaurants.bind(this);
         this.getSavedRestaurants();
-
     }
 
     signIn() {
@@ -256,24 +43,12 @@ class App extends React.Component {
         auth.signInWithPopup(provider)
             .then((result) => {
                 const user = result.user;
+                this.getSavedRestaurants();
                 this.setState({
                     user
                 })
                 console.log(user)
             })
-        // const provider = new firebase.auth.GoogleAuthProvider();
-
-        // provider.setCustomParameters({
-        //     prompt: 'select_account'
-        // })
-        // firebase.auth().signInWithPopup(provider)
-        //     .then((user) => {
-        //         console.log(user);
-        //         const user = result.user;
-        //         this.setState ({
-        //             user
-        //         })
-        //     })
     }
     signOut() {
         auth.signOut()
@@ -282,7 +57,6 @@ class App extends React.Component {
                     user: null
                 });
             });
-        // firebase.auth().signOut();
     }
     handleChange(e) {
         this.setState({
@@ -293,14 +67,6 @@ class App extends React.Component {
         const dbRef = firebase.database().ref('/restaurants');
 
         const restaurantList = [];
-
-        // dbref.on('value', (snapshot) => {
-        //     const data = snapshot.val();
-        //     const state = [];
-        //     for (let key in data) {
-        //         data[key].key = key;
-        //         state.push(data[key]);
-        //     }
 
         dbRef.on('value', (response) => {
             const data = response.val();
@@ -352,11 +118,8 @@ class App extends React.Component {
                                 address: eatingPlace.restaurant.location.address,
                                 latitude: eatingPlace.restaurant.location.latitude,
                                 longitude: eatingPlace.restaurant.location.longitude,
-                                // rating: eatingPlace.restaurant.user_rating.aggregate_rating
                             };
                         });
-                        // console.log(newArray);
-
                         this.setState({
                             restaurants: newArray,
                             lat: data.results[0].geometry.location.lat,
@@ -376,6 +139,10 @@ class App extends React.Component {
         const coords = {};
 
         this.getCoords(inputResult);
+
+        $('html, body').animate({
+            scrollTop: $('#map').offset().top
+        });
     }
     render() {
 
@@ -399,16 +166,6 @@ class App extends React.Component {
                         </span>
                       </div>
                     </div>
-                    {/* <form onSubmit={this.submit} className="wrapper">
-                    <label htmlFor="userSearch">City or Address:</label>
-                    <input type="text" id="userText" value={this.state.userText} onChange={this.handleChange} />
-                    <input type="submit" value="Food Me!" />
-                  </form>
-
-                  <div id="map" className="map">
-
-                    <MapContainer locations={this.state.restaurants} coords={this.state.coordinates} />
-                  </div>     */}
                   </div>
                 </div> : <div className="wrapper" />}
               {this.state.user ? <button className="authButton logOut" onClick={this.signOut}>
--- dev/scripts/google.js
+++ dev/scripts/google.js
@@ -1,175 +1,12 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
-// // import Sidebar from './sidebar'
-
-
-// export class MapContainer extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             title: '',
-//             // rating:'',
-//             address: '',
-//             showingInfoWindow: false,
-//             selectedPlace: {},
-//             activeMarker: {},
-//             loggedIn: false,
-//             savedRestaurants: props.userHistory
-//         }
-        
-//         this.markerClick = this.markerClick.bind(this);
-//         this.onMapClicked = this.onMapClicked.bind(this);
-//         this.clickThis = this.clickThis.bind(this);
-//         this.deleteRestaurant = this.deleteRestaurant.bind(this);
-
-//     }
-
- 
-//     componentWillReceiveProps(props) {
-//         // console.log(this.props.userHistory)
-//         console.log(props)
-//         let restaurantHistory = props.userHistory
-//         this.setState({
-//             savedRestaurants: restaurantHistory
-//         })
-//     }
-//     markerClick(props, marker) {
-
-//         this.setState({
-//             showingInfoWindow: true,
-//             title: props.title,
-//             activeMarker: marker,
-//             address: props.address
-//         })
-//     }
-//     clickThis(){
-//         const userSave = {
-//             restaurant: this.state.title,
-//             address: this.state.address
-//         }
-
-//         const dbRef = firebase.database().ref('/restaurants');
-//         dbRef.push(userSave);     
-//     }
-//     componentDidMount() {
-//         const dbRef = firebase.database().ref('/restaurants');
-//         dbRef.on('value', (snapshot)=>{
-//             let items = snapshot.val();
-//             let newState = [];
-//             for (let item in items) {
-//                 newState.push({
-//                     id: item,
-//                     title: items[item].title,
-//                     address: items[item].address
-//                 });
-//             }
-//             this.setState({
-//                 places:newState
-//             });
-//             // console.log(items);
-//         });
-//     }
-
-//     onMapClicked(props) {
-//         if (this.state.showingInfoWindow) {
-//             this.setState({
-//                 showingInfoWindow: false,
-//                 activeMarker: null,
-//             })
-//         }
-//     }
-//     deleteRestaurant(key) {
-//         // e.preventDefault();
-//         // console.log(key)
-//         const dbRef = firebase.database().ref(`restaurants${key}`)
-//         dbRef.remove();
-//     }
-//     render(props) {
-//         const style = {
-//             width:'70%',
-//             height:'80%'
-//         }
-//         return (<div className="rightColumn">
-//             <div className="infoPane">
-//                     <button onClick={this.clickThis} className="save">Save Restaurant</button>
-
-//                 {this.state.savedRestaurants.map((restaurant) => {
-//                     console.log(restaurant)
-//                     return(
-//                     <span key={restaurant.key}>
-//                         <h5>{restaurant.restaurant}</h5>
-//                         <p>{restaurant.address}</p>
-//                         <button value={restaurant.key} onClick={() => this.deleteRestaurant(restaurant.key)}><i class="fas fa-times"></i></button>
-//                     </span>                        
-//                     )                    
-//                 })}
-//             </div>
-//             <section className="saved">
-//                 <div className="wrapper">
-                
-
-//                 </div>
-//             </section>
-            
-//             <Map google={this.props.google} zoom={13} onClick={this.onMapClicked} center={this.props.coords} style={style}>
-//               {Object.values(this.props.locations).map(
-//                 (location, i) => {
-//                   return (
-//                     <Marker
-//                       name={"Toronto"}
-//                       title={location.name}
-//                       address={location.address}
-//                       position={{
-//                         lat: location.latitude,
-//                         lng: location.longitude
-//                       }}
-//                       onClick={this.markerClick}
-//                       name={"Current location"}
-//                       key={i}
-//                     />
-//                   );
-//                 }
-//               )}
-//               <InfoWindow marker={this.state.activeMarker} onClose={this.onInfoWindowClose} visible={this.state.showingInfoWindow}>
-//                 <div className="results">
-//                   <h2>{this.state.title}</h2>
-//                   <p className="locationAddress">
-//                     {this.state.address}
-//                   </p>
-//                 </div>
-//               </InfoWindow>
-//             </Map>
-            
-//             {/* <div className="infopaneContainer">
-//               {this.state.restaurants.map((rest, i) => {
-//                   return (
-//                       <Sidebar title={this.state.title} address={this.state.address} click={this.clickThis} delete={this.deleteRestaurant} rest={this.state.restaurants} key={i}/>
-//                   )
-//               })}
-//             </div> */}
-//           </div>
-//         )
-//     }
-// }
-
-// export default GoogleApiWrapper({
-//     apiKey: ('AIzaSyCNX1tthuQLaX98UVGv2dcbFnpjdhw0TnQ')
-// })(MapContainer)
-
-// WORKING CODE
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
-
 
 export class MapContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             title: '',
-            // rating:'',
             address: '',
             showingInfoWindow: false,
             selectedPlace: {},
@@ -182,9 +19,7 @@ export class MapContainer extends React.Component {
         this.onMapClicked = this.onMapClicked.bind(this);
         this.clickThis = this.clickThis.bind(this);
         this.deleteRestaurant = this.deleteRestaurant.bind(this);
-
     }
-
 
     componentWillReceiveProps(props) {
         // console.log(this.props.userHistory)
@@ -200,23 +35,14 @@ export class MapContainer extends React.Component {
             showingInfoWindow: true,
             title: props.title,
             activeMarker: marker,
-
-            address: props.address,
-
-
-
-
+            address: props.address
         })
 
     }
     clickThis() {
-
-
         const userSave = {
             restaurant: this.state.title,
             address: this.state.address,
-
-
         }
 
         const dbRef = firebase.database().ref('/restaurants');
@@ -263,9 +89,11 @@ export class MapContainer extends React.Component {
         return <div className="rightColumn">
             <div className="infoPane">
               <h3>{this.props.userInfo}'s Picks</h3>
-              <button onClick={this.clickThis} className="save">
-                Save Restaurant
-              </button>
+              <div>
+                  <button onClick={this.clickThis} className="save">
+                    Save Restaurant
+                  </button>
+              </div>
 
               {this.state.savedRestaurants.map(restaurant => {
                 console.log(restaurant);
@@ -282,7 +110,7 @@ export class MapContainer extends React.Component {
               <div className="wrapper" />
             </section>
 
-            <Map google={this.props.google} centerAroundCurrentLocation={true} zoom={14} onClick={this.onMapClicked} center={this.props.coords} style={style}>
+            <Map google={this.props.google} centerAroundCurrentLocation={true} zoom={16} onClick={this.onMapClicked} center={this.props.coords} style={style}>
               {Object.values(this.props.locations).map(
                 (location, i) => {
                   return (
